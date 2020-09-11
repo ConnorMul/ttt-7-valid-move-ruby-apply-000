2103df74f474c76ffedc3679f22c2bd992d035b7
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-11 04:41:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken? == true && index <= 8
+  if (board[index] == " " || board[index] == "" || board[index] == nil) && index <= 8
     return true 
   else 
     return false 
